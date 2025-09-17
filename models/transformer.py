c6900947286e8caf9b29fa3ed9227ec4d3877670
amirhosein afshin <108872227+amirhoseinafshin001@users.noreply.github.com>
2025-09-17 04:31:24 -0700

    transformer model init and training

---
+++ models/transformer.py
@@ -0,0 +1,46 @@
+from typing import Literal
+import torch as t
+from torch import nn
+from transformers import AutoModel
+
+
+class EmotionClassifierTransformer(nn.Module):
+    def __init__(self,
+                mode: Literal ['feature-extract', 'fine-tune'],
+                model_name="distilbert-base-uncased",
+                num_labels=28,
+                dropout=0.3
+        ) -> None:
+        super().__init__()
+        self.encoder = AutoModel.from_pretrained(model_name)
+        
+        match mode:
+            case 'feature-extract':
+                # Feature Extraction means we only train a head to work with embedding vecs
+                # So we need to freeze our transformer model.
+                for param in self.encoder.parameters():
+                    param.requires_grad = False
+            case 'fine-tune':
+                # Fine Tuning is when we train the encoder transformer too (usually with a lower LR)
+                # It's more accurate, But also slower. We enable gradients here.
+                for param in self.encoder.parameters():
+                    param.requires_grad = True
+            case _:
+                raise ValueError(f'undefined mode {mode}')
+        
+        self.head = nn.Sequential(
+            # nn.Linear(self.encoder.config.hidden_size, self.encoder.config.hidden_size),
+            # nn.ReLU(),      # these two layers are added just in case we wanted to make the head deeper
+            nn.Dropout(dropout),
+            nn.Linear(self.encoder.config.hidden_size, num_labels),
+            # nn.Sigmoid()    # multi-label classification (commented so we use BCEWithLogits criterion)
+        )
+
+    def forward(self,
+                input_ids: t.Tensor,
+                attention_mask: t.Tensor
+        ) -> t.Tensor:
+        embedding_vec = self.encoder(input_ids=input_ids, attention_mask=attention_mask)
+        pooled = embedding_vec.last_hidden_state[:, 0, :]   # CLS token representation
+        return self.head(pooled)                            # this is the final classification result.
+    
--- train.py
+++ train.py
@@ -15,7 +15,6 @@ def train_bilstm(model: nn.Module, dl: t.utils.data.DataLoader, lr: float, epoch
         progress_bar = tqdm(dl, desc=f"Epoch {epoch+1}/{epochs}", leave=True)
 
         for batch in progress_bar:
-            # x, y = batch[0].to(device), batch[1].to(device)
             x = batch["input_ids"].to(device)
             mask = batch["attention_mask"].to(device)
             y = batch["labels"].to(device)
@@ -25,23 +24,94 @@ def train_bilstm(model: nn.Module, dl: t.utils.data.DataLoader, lr: float, epoch
             optim.zero_grad()
             loss.backward()
             optim.step()
-
             
             total_epoch_loss += loss.item()
             progress_bar.set_postfix(loss=loss.item())        
         print(f'epoch {epoch+1} | loss: {total_epoch_loss:.4f}')
 
 
+def train_transformer(model: nn.Module, dl: t.utils.data.DataLoader, lr: float, epochs: int, mode, device, encoder_lr=None):
+    model.to(device=device)
+    model.train()
+
+    if mode=='feature-extract':
+        optim = t.optim.Adam(model.head.parameters(), lr=lr)
+        criterion = nn.BCEWithLogitsLoss()
+    elif mode=='fine-tune':
+        optimizer_grouped_parameters = [
+            {"params": model.encoder.parameters(), "lr": encoder_lr},
+            {"params": model.head.parameters(), "lr": lr}
+        ]
+        optim = t.optim.AdamW(optimizer_grouped_parameters)     # AdamW works better with transformers
+        criterion = nn.BCEWithLogitsLoss()
+    else:
+        raise ValueError(f'undefined mode: {mode}')
+
+    for epoch in range(epochs):
+        total_epoch_loss = 0
+        progress_bar = tqdm(dl, desc=f"Epoch {epoch+1}/{epochs}", leave=True)
+
+        for batch in progress_bar:
+            x = batch["input_ids"].to(device)
+            mask = batch["attention_mask"].to(device)
+            y = batch["labels"].to(device)
+
+            logit = model(x, mask)
+            loss = criterion(logit, y.float())
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            total_epoch_loss += loss.item()
+            progress_bar.set_postfix(loss=loss.item())        
+        print(f'epoch {epoch+1} | loss: {total_epoch_loss:.4f}')
+
+
 if __name__=="__main__":
+    from sys import argv, exit
+
     from models.bilstm import EmotionClassifierBiLSTM
+    from models.transformer import EmotionClassifierTransformer
     from data.preprocess import get_dataloaders
     from utils.text import get_vocab_size
-    from config import B, LR, EPOCHS
+    from config import B, LR, EPOCHS, FINETUNE_LR
 
     device = 'cuda' if t.cuda.is_available() else 'cpu'
-    train_dl, val_dl, test_dl = get_dataloaders(batch_size=B)
-    
-    model = EmotionClassifierBiLSTM(vocab_size=get_vocab_size())
-    print('training started.')
-    train_bilstm(model, train_dl, LR, EPOCHS, 'max-pool', device)
-    t.save(model.state_dict(), "checkpoints/bilstm_maxpool.pt")
+
+    if len(argv) != 3:
+        print('Usage: python train.py <model> <method>')
+        exit(1)
+
+    match (argv[1], argv[2]):
+        case ('bilstm', 'max-pool'):
+            train_dl, val_dl, test_dl = get_dataloaders(batch_size=B)
+            model = EmotionClassifierBiLSTM(vocab_size=get_vocab_size())
+            print('training started.')
+            train_bilstm(model, train_dl, LR, EPOCHS, 'max-pool', device)
+            t.save(model.state_dict(), "checkpoints/bilstm_maxpool.pt")
+        
+        case ('bilstm', 'last-token'):
+            train_dl, val_dl, test_dl = get_dataloaders(batch_size=B)
+            model = EmotionClassifierBiLSTM(vocab_size=get_vocab_size())
+            print('training started.')
+            train_bilstm(model, train_dl, LR, EPOCHS, 'last-token', device)
+            t.save(model.state_dict(), "checkpoints/bilstm_lasttoken.pt")
+        
+        case ('transformer', 'feature-extract'):
+            train_dl, val_dl, test_dl = get_dataloaders(batch_size=B, tokenizer_name='distilbert-base-uncased')
+            model = EmotionClassifierTransformer('feature-extract')
+            print('training started.')
+            train_transformer(model, train_dl, LR, EPOCHS, 'feature-extract', device)
+            model.encoder.config.save_pretrained("checkpoints/bert")
+            t.save(model.state_dict(), "checkpoints/transformer_featureextract.pt")
+        
+        case ('transformer', 'fine-tune'):
+            train_dl, val_dl, test_dl = get_dataloaders(batch_size=B, tokenizer_name='distilbert-base-uncased')
+            model = EmotionClassifierTransformer('fine-tune')
+            print('training started.')
+            train_transformer(model, train_dl, LR, EPOCHS, 'fine-tune', device, FINETUNE_LR)
+            model.encoder.config.save_pretrained("checkpoints/bert")
+            t.save(model.state_dict(), "checkpoints/transformer_finetune.pt")
+        
+        case _:
+            print('Undefined model and/or method.')
